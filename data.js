e79cbac4db247b8e623faafdbf3cea38040c67a7
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-08-04 12:14:50 +0300

    Add file JSON

--- data.js
+++ data.js
@@ -1,1 +1,32 @@
-
+const coinDesk = `{
+  "time": {
+    "updated": "Aug 4, 2024 09:01:49 UTC",
+    "updatedISO": "2024-08-04T09:01:49+00:00",
+    "updateduk": "Aug 4, 2024 at 10:01 BST"
+  },
+  "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+  "chartName": "Bitcoin",
+  "bpi": {
+    "USD": {
+      "code": "USD",
+      "symbol": "&#36;",
+      "rate": "60,691.563",
+      "description": "United States Dollar",
+      "rate_float": 60691.5633
+    },
+    "GBP": {
+      "code": "GBP",
+      "symbol": "&pound;",
+      "rate": "47,393.071",
+      "description": "British Pound Sterling",
+      "rate_float": 47393.0707
+    },
+    "EUR": {
+      "code": "EUR",
+      "symbol": "&euro;",
+      "rate": "55,596.203",
+      "description": "Euro",
+      "rate_float": 55596.2031
+    }
+  }
+}`;
